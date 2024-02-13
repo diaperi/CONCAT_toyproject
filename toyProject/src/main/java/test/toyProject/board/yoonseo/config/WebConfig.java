5dacec94a4033544b1618609149a697b0f7148f0
yoonseo <choiys613@gmail.com>
2024-02-13 16:59:38 +0900

    해결

--- toyProject/src/main/java/test/toyProject/board/yoonseo/config/WebConfig.java
+++ toyProject/src/main/java/test/toyProject/board/yoonseo/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String resourcePath = "/upload/**"; // view 에서 접근할 경로
-    private String savePath = "file:///C:/yoonseo_igit mg/"; // 실제 파일 저장 경로
+    private String savePath = "file:///C:/yoonseo_img/"; // 실제 파일 저장 경로
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
